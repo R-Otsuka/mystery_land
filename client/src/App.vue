59ea7cba6031c5199fbcf210c542a85a9365e7f8
kamio06 <yakugozy@gmail.com>
2020-10-29 23:03:22 +0900

    サイドバーの各項目に適切なroutingを割り当てる

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,21 +1,52 @@
 <template>
-  <v-app>
+  <div>
+    <nav>
+      <v-content>
+        <v-toolbar app flat>
+          <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+          <v-toolbar-title>Question</v-toolbar-title>
+          <v-spacer></v-spacer>
+          <v-btn icon>
+            <v-icon>mdi-keyboard-backspace</v-icon>
+          </v-btn>
+        </v-toolbar>
+      </v-content>
+
+      <v-navigation-drawer v-model="drawer" app class="success">
+        <v-list-item v-for="link in links" :key="link.text" router :to="link.route">
+          <v-list-item-icon>
+            <v-icon class="white--text" v-if=link.icon>
+              {{ icon.unlock }}
+            </v-icon>
+            <v-icon class="white--text" v-else>
+              {{ icon.lock }}
+            </v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title class="white--text">{{ link.text }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-navigation-drawer>
+    </nav>
     <v-content>
       <router-view></router-view>
     </v-content>
-  </v-app>
+  </div>
 </template>
 
 <script>
-
 export default {
-  name: 'App',
-
-  components: {
-  },
-
-  data: () => ({
-    //
+  data:() =>({
+    drawer:false,
+    icon:{
+      "unlock":"mdi-lock-open-variant",
+      "lock":"mdi-lock",
+    },
+    links: [
+      {icon:true, text:'Q1',route:'/sample/1'},
+      {icon:true, text:'Q2',route:'/sample/2'},
+      {icon:false, text:'Q3',route:'/sample/3'},
+    ]
   }),
-};
+}
 </script>
--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,29 +1,42 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
-import Sample from '../views/sample'
+import SampleHome from '../views/sample_list/home'
+import Sample1 from '../views/sample_list/sample1'
+import Sample2 from '../views/sample_list/sample2'
+import Sample3 from '../views/sample_list/sample3'
 
 Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
+
+  // {
+  //   path: '/about',
+  //   name: 'About',
+  //   // route level code-splitting
+  //   // this generates a separate chunk (about.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  // }
   {
     path: '/sample',
-    name: 'Sample',
-    component: Sample
+    name: 'SampleHome',
+    component: SampleHome
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    path: '/sample/1',
+    name: 'Sample1',
+    component: Sample1
+  },
+  {
+    path: '/sample/2',
+    name: 'Sample2',
+    component: Sample2
+  },
+  {
+    path: '/sample/3',
+    name: 'Sample3',
+    component: Sample3
+  },
 ]
 
 const router = new VueRouter({
