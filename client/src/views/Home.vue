595febcbea0fe1cb7ec389dc9e0c45280d9214a6
kamio06 <yakugozy@gmail.com>
2020-10-29 17:03:47 +0900

    ヘッダーとサイドバーの雛形作成

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,18 +1,30 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>This is HomePage</h1>
+    <p class="red blue--text">
+      いかがでしたでしょうか。 Vuetifyなどのデザインフレームワークを使わずにスクラッチでデザインを構築する場合も、Vuetifyの命名規則や構成はとても参考になると思います！是非一度使ってみて下さい。
+      今回は以上になります。最後までお読み頂きありがとうございました！
+    </p>
+    <p class="pink lighten-4 red--text text--darken4">
+      hello
+    </p>
+    <div class="display-4">
+      Massive
+    </div>
+    <v-row
+        align="center"
+        justify="space-around"
+    >
+      <v-btn class="pink white--text">Click Me</v-btn>
+      <v-btn depressed color="pink">Click Me</v-btn>
+    </v-row>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+
 }
+
 </script>
---
+++ client/src/views/sample.vue
@@ -0,0 +1,29 @@
+<template>
+  <nav>
+    <v-toolbar color="primary" dark="true">
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title>Title</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn icon>
+        <v-icon>mdi-keyboard-backspace</v-icon>
+      </v-btn>
+    </v-toolbar>
+
+    <v-navigation-drawer v-model="drawer" app class="success">
+      <v-list-item>
+        <v-list-item-icon>
+          <v-icon></v-icon>
+          <v-list-item-content></v-list-item-content>
+        </v-list-item-icon>
+      </v-list-item>
+    </v-navigation-drawer>
+  </nav>
+</template>
+
+<script>
+export default {
+  data:() =>({
+    drawer:false,
+  }),
+}
+</script>
