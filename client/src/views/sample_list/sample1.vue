da43af0ed0f32436af972fb5c0e536059d0fde81
kamio06 <yakugozy@gmail.com>
2020-11-05 04:44:21 +0900

    シャッフル機能の修正と消滅時アニメーション追加

--- client/src/views/sample_list/sample1.vue
+++ client/src/views/sample_list/sample1.vue
@@ -3,58 +3,68 @@
 <!--  <v-content class="py-10">-->
   <v-container>
     <div class="mx-auto text-center py-5">
-      <v-responsive class="rounded-circle d-inline-flex"
-                    :class=items[0].color
-                    :style="{visibility: items[0].visibility ? 'visible' : 'hidden'}"
-                    @click="hide(0)"
-                    height="64" width="64">
-      </v-responsive>
+      <transition>
+        <span class="rounded-circle d-inline-flex"
+              :class=items[0].color
+              @click="hide(0)"
+              v-show="items[0].visibility"
+              :style="{opacity: items[0].visibility ? 1 : 0}"
+        >
+        </span>
+      </transition>
     </div>
 <!--      start,center,など設定によりseparateを変えられる(公式参照)-->
     <v-row justify="space-around" class="py-5">
       <v-col cols="3" class="text-sm-left">
-        <v-responsive class="rounded-circle d-inline-flex"
-                      v-bind:class=items[1].color
-                      :style="{visibility: items[1].visibility ? 'visible' : 'hidden'}"
-                      @click="hide(1)"
-                      height="64" width="64">
-
-        </v-responsive>
+        <transition>
+          <span class="rounded-circle d-inline-flex"
+                v-bind:class=items[1].color
+                @click="hide(1)"
+                v-show="items[1].visibility"
+                :style="{opacity: items[1].visibility ? 1 : 0}"
+          >
+          </span>
+        </transition>
       </v-col>
       <v-col cols="3" class="text-sm-right">
-        <v-responsive class="rounded-circle d-inline-flex"
-                      v-bind:class=items[2].color
-                      :style="{visibility: items[2].visibility ? 'visible' : 'hidden'}"
-                      @click="hide(2)"
-                      height="64" width="64">
-
-        </v-responsive>
+        <transition>
+          <span class="rounded-circle d-inline-flex"
+                v-bind:class=items[2].color
+                @click="hide(2)"
+                v-show="items[2].visibility"
+                :style="{opacity: items[2].visibility ? 1 : 0}"
+          >
+          </span>
+        </transition>
       </v-col>
     </v-row>
     <p class="mx-auto text-center pb-10" style="width: 200px;">⠁⠡⠁⠊⠩⠚⠷⠐⠞⠓</p>
     <v-row justify="center" class="py-5">
       <v-col cols="3" class="text-sm-left">
-        <v-responsive class="rounded-circle d-inline-flex"
-                      v-bind:class=items[3].color
-                      :style="{visibility: items[3].visibility ? 'visible' : 'hidden'}"
-                      @click="hide(3)"
-                      height="64" width="64">
-
-        </v-responsive>
+        <transition>
+          <span class="rounded-circle d-inline-flex"
+                v-bind:class=items[3].color
+                @click="hide(3)"
+                v-show="items[3].visibility"
+                :style="{opacity: items[3].visibility ? 1 : 0}"
+          >
+          </span>
+        </transition>
       </v-col>
       <v-col cols="3" class="text-sm-right">
-        <v-responsive class="rounded-circle d-inline-flex"
-                      v-bind:class=items[4].color
-                      :style="{visibility: items[4].visibility ? 'visible' : 'hidden'}"
-                      @click="hide(4)"
-                      height="64" width="64">
-
-        </v-responsive>
+        <transition>
+          <span class="rounded-circle d-inline-flex"
+                v-bind:class=items[4].color
+                @click="hide(4)"
+                v-show="items[4].visibility"
+                :style="{opacity: items[4].visibility ? 1 : 0}"
+          >
+          </span>
+        </transition>
       </v-col>
     </v-row>
-    <button @click="startshuffle">shuffle</button>
+    <button @click="listShuffle">shuffle</button>
     <br>
-    <button @click="stopshuffle">stop</button>
   </v-container>
 <!--  </v-content>-->
 </template>
@@ -63,7 +73,6 @@ import _ from 'lodash';
 export default {
   data:()=> ({
     setfunc:false,
-    showOrNot:false,
     items:[
       {id:0,color:'warning',visibility:true},
       {id:1,color:'purple',visibility:true},
@@ -73,24 +82,52 @@ export default {
     ]
   }),
   methods: {
-    onShuffleClick() {
+    listShuffle() {
       this.items = _.shuffle(this.items);
     },
-    startshuffle(){
-      if(this.setfunc == true){
-        setTimeout(this.startshuffle, 3000);
-        this.onShuffleClick()
-      }else{
-        this.setfunc = true
+    hide(id){
+      if(this.items[id].visibility == true) {
+        this.items[id].visibility = !this.items[id].visibility
+        console.log(this.items[id].visibility)
       }
     },
-    stopshuffle(){
-      this.setfunc = false
-    },
-    hide(id){
-      this.items[id].visibility = !this.items[id].visibility
-      //そいつのvisibilityをfalseに変更、しかし順番あり
-    }
   }
 }
 </script>
+<style>
+.rounded-circle {
+  border-radius: 50%;
+  width: 64px;
+  height: 64px;
+  background: #000;
+}
+.v-leave-active{
+  transition: opacity 1s;
+}
+.v-enter,
+.v-leave-to {
+  opacity: 0;
+}
+@keyframes v-in {
+  0% {
+    transform: scale(0);
+  }
+  50% {
+    transform: scale(1.5);
+  }
+  100% {
+    transform: scale(1);
+  }
+}
+@keyframes v-out {
+  0% {
+    transform: scale(1);
+  }
+  50% {
+    transform: scale(1.5);
+  }
+  100% {
+    transform: scale(0);
+  }
+}
+</style>
