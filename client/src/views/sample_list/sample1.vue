7362a7921ed07a35f1174184eb19565592da055f
kamio06 <yakugozy@gmail.com>
2020-11-04 13:50:37 +0900

    lodashによるlistシャッフル機能の追加

--- client/src/views/sample_list/sample1.vue
+++ client/src/views/sample_list/sample1.vue
@@ -1,33 +1,49 @@
 <template>
-  <v-content class="py-10">
+<!--  v-contentタグはサイドバー表示の際にpadding-leftの自動追加機能あり??-->
+<!--  <v-content class="py-10">-->
+  <v-container>
     <div class="mx-auto text-center py-5">
-      <v-responsive class=" rounded-circle d-inline-flex success" height="64" width="64"></v-responsive>
+      <v-responsive class=" rounded-circle d-inline-flex" v-bind:class=items[0].color height="64" width="64"></v-responsive>
     </div>
-    <v-container>
 <!--      start,center,など設定によりseparateを変えられる(公式参照)-->
-      <v-row justify="space-around" class="py-5">
-        <v-col cols="3" class="text-sm-left">
-          <v-responsive class="rounded-circle d-inline-flex primary lighten-2 text-sm-left" height="64" width="64"></v-responsive>
-        </v-col>
-        <v-col cols="3" class="text-sm-right">
-          <v-responsive class="rounded-circle d-inline-flex grey lighten-2" height="64" width="64" ></v-responsive>
-        </v-col>
-      </v-row>
-      <p class="mx-auto text-center pb-10" style="width: 200px;">⠁⠡⠁⠊⠩⠚⠷⠐⠞⠓</p>
-      <v-row justify="center" class="py-5">
-        <v-col cols="3" class="text-sm-left">
-          <v-responsive class="rounded-circle d-inline-flex purple lighten-2" height="64" width="64"></v-responsive>
-        </v-col>
-        <v-col cols="3" class="text-sm-right">
-          <v-responsive class="rounded-circle d-inline-flex warning lighten-2" height="64" width="64"></v-responsive>
-        </v-col>
-      </v-row>
-    </v-container>
-  </v-content>
+    <v-row justify="space-around" class="py-5">
+      <v-col cols="3" class="text-sm-left">
+        <v-responsive class="rounded-circle d-inline-flex lighten-2 text-sm-left" v-bind:class=items[1].color height="64" width="64"></v-responsive>
+      </v-col>
+      <v-col cols="3" class="text-sm-right">
+        <v-responsive class="rounded-circle d-inline-flex lighten-2" v-bind:class=items[2].color height="64" width="64" ></v-responsive>
+      </v-col>
+    </v-row>
+    <p class="mx-auto text-center pb-10" style="width: 200px;">⠁⠡⠁⠊⠩⠚⠷⠐⠞⠓</p>
+    <v-row justify="center" class="py-5">
+      <v-col cols="3" class="text-sm-left">
+        <v-responsive class="rounded-circle d-inline-flex lighten-2" v-bind:class=items[3].color height="64" width="64"></v-responsive>
+      </v-col>
+      <v-col cols="3" class="text-sm-right">
+        <v-responsive class="rounded-circle d-inline-flex warning lighten-2" v-bind:class=items[4].color height="64" width="64"></v-responsive>
+      </v-col>
+    </v-row>
+    <button @click="onShuffleClick">shuffle</button>
+  </v-container>
+<!--  </v-content>-->
 </template>
-
 <script>
+import _ from 'lodash';
 export default {
-
+  el: '#flip-list-demo',
+  data:()=> ({
+    items:[
+      {id:0,color:'warning'},
+      {id:1,color:'purple'},
+      {id:2,color:'grey'},
+      {id:3,color:'primary'},
+      {id:4,color:'success'}
+    ]
+  }),
+  methods: {
+    onShuffleClick() {
+      this.items = _.shuffle(this.items);
+    }
+  }
 }
 </script>
