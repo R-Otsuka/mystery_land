9d6c82938f81b63e00f49a2eca6951e488e076d0
kamio06 <yakugozy@gmail.com>
2020-11-05 02:22:27 +0900

    classの修正+クリックでボールが消えるように仕様追加

--- client/src/views/sample_list/sample1.vue
+++ client/src/views/sample_list/sample1.vue
@@ -3,24 +3,53 @@
 <!--  <v-content class="py-10">-->
   <v-container>
     <div class="mx-auto text-center py-5">
-      <v-responsive class=" rounded-circle d-inline-flex" v-bind:class=items[0].color height="64" width="64"></v-responsive>
+      <v-responsive class="rounded-circle d-inline-flex"
+                    :class=items[0].color
+                    :style="{visibility: items[0].visibility ? 'visible' : 'hidden'}"
+                    @click="hide(0)"
+                    height="64" width="64">
+      </v-responsive>
     </div>
 <!--      start,center,など設定によりseparateを変えられる(公式参照)-->
     <v-row justify="space-around" class="py-5">
       <v-col cols="3" class="text-sm-left">
-        <v-responsive class="rounded-circle d-inline-flex lighten-2 text-sm-left" v-bind:class=items[1].color height="64" width="64"></v-responsive>
+        <v-responsive class="rounded-circle d-inline-flex"
+                      v-bind:class=items[1].color
+                      :style="{visibility: items[1].visibility ? 'visible' : 'hidden'}"
+                      @click="hide(1)"
+                      height="64" width="64">
+
+        </v-responsive>
       </v-col>
       <v-col cols="3" class="text-sm-right">
-        <v-responsive class="rounded-circle d-inline-flex lighten-2" v-bind:class=items[2].color height="64" width="64" ></v-responsive>
+        <v-responsive class="rounded-circle d-inline-flex"
+                      v-bind:class=items[2].color
+                      :style="{visibility: items[2].visibility ? 'visible' : 'hidden'}"
+                      @click="hide(2)"
+                      height="64" width="64">
+
+        </v-responsive>
       </v-col>
     </v-row>
     <p class="mx-auto text-center pb-10" style="width: 200px;">⠁⠡⠁⠊⠩⠚⠷⠐⠞⠓</p>
     <v-row justify="center" class="py-5">
       <v-col cols="3" class="text-sm-left">
-        <v-responsive class="rounded-circle d-inline-flex lighten-2" v-bind:class=items[3].color height="64" width="64"></v-responsive>
+        <v-responsive class="rounded-circle d-inline-flex"
+                      v-bind:class=items[3].color
+                      :style="{visibility: items[3].visibility ? 'visible' : 'hidden'}"
+                      @click="hide(3)"
+                      height="64" width="64">
+
+        </v-responsive>
       </v-col>
       <v-col cols="3" class="text-sm-right">
-        <v-responsive class="rounded-circle d-inline-flex warning lighten-2" v-bind:class=items[4].color height="64" width="64"></v-responsive>
+        <v-responsive class="rounded-circle d-inline-flex"
+                      v-bind:class=items[4].color
+                      :style="{visibility: items[4].visibility ? 'visible' : 'hidden'}"
+                      @click="hide(4)"
+                      height="64" width="64">
+
+        </v-responsive>
       </v-col>
     </v-row>
     <button @click="startshuffle">shuffle</button>
@@ -33,13 +62,14 @@
 import _ from 'lodash';
 export default {
   data:()=> ({
-    setfunc:true,
+    setfunc:false,
+    showOrNot:false,
     items:[
-      {id:0,color:'warning'},
-      {id:1,color:'purple'},
-      {id:2,color:'grey'},
-      {id:3,color:'primary'},
-      {id:4,color:'success'}
+      {id:0,color:'warning',visibility:true},
+      {id:1,color:'purple',visibility:true},
+      {id:2,color:'grey',visibility:true},
+      {id:3,color:'primary',visibility:true},
+      {id:4,color:'success',visibility:true}
     ]
   }),
   methods: {
@@ -56,6 +86,10 @@ export default {
     },
     stopshuffle(){
       this.setfunc = false
+    },
+    hide(id){
+      this.items[id].visibility = !this.items[id].visibility
+      //そいつのvisibilityをfalseに変更、しかし順番あり
     }
   }
 }
