1a46173075ab49eeb6ebe2823af0bc047a0a7dfb
kamio06 <yakugozy@gmail.com>
2020-11-11 20:42:00 +0900

    Q2で使用する風船のレイアウト作成

--- client/src/views/sample_list/sample1.vue
+++ client/src/views/sample_list/sample1.vue
@@ -108,7 +108,7 @@ export default {
   }
 }
 </script>
-<style>
+<style scoped>
 .tenji{
   padding-top:10px;
   padding-bottom:15px;
--- client/src/views/sample_list/sample2.vue
+++ client/src/views/sample_list/sample2.vue
@@ -1,79 +1,103 @@
 <template>
-  <div id="app">
-    <button @click="toggleBtn">表示／非表示ボタン</button>
-    <transition name="simple">
-      <p v-show="show" style="width:150px;height:150px;background-color:green">本日は晴天なり</p>
-    </transition>
-    <button @click="count++">+ju</button>
-    <transition name="datachange">
-      <div :key="count">{{ count }}</div>
-    </transition>
-    <button @click="order=!order">切り替え</button>
-    <transition-group name="group" tag="ul" class="list">
-      <li v-for="item in sortedList" v-bind:key="item.id">
-        {{ item.name }} {{ item.price }}円
-      </li>
-    </transition-group>
-    <button @click="change=!change">切り替え</button>
-    <transition name="hook" v-on:enter="enter" v-on:after-enter="afterEnter">
-      <div v-if="change">example</div>
-    </transition>
+  <div>
+  <!--    16個数の風船-->
+    <div>
+<!--      <transition-group name="cell" tag="div" class="container">-->
+<!--        <div v-for="cell in cells" :key="cell.id" class="cell">-->
+<!--          {{ cell.number }}-->
+<!--        </div>-->
+<!--      </transition-group>-->
+<!--      <div v-for="number in numbers1"-->
+<!--           v-bind:key="number.id"-->
+<!--           class="rounded-circle text-center float-left"-->
+<!--           @click="judgeAndHide(number.id)"-->
+<!--           v-show="number.visibility">-->
+<!--        {{ number.num }}-->
+<!--      </div>-->
+      <v-row justify="center">
+        <v-col cols="3" v-for="number in numbers1" v-bind:key="number.id">
+          <div class="rounded-circle mx-auto balloon">
+            <span>{{ number.num }}</span>
+          </div>
+        </v-col>
+      </v-row>
+      <v-row justify="center">
+        <v-col cols="3" v-for="number in numbers2" v-bind:key="number.id">
+          <div class="rounded-circle mx-auto balloon">
+            {{ number.num }}
+          </div>
+        </v-col>
+      </v-row>
+      <v-row justify="center">
+        <v-col cols="3" v-for="number in numbers3" v-bind:key="number.id">
+          <div class="rounded-circle mx-auto balloon">
+            {{ number.num }}
+          </div>
+        </v-col>
+      </v-row>
+      <v-row justify="center">
+        <v-col cols="3" v-for="number in numbers4" v-bind:key="number.id">
+          <div class="rounded-circle mx-auto balloon">
+            {{ number.num }}
+          </div>
+        </v-col>
+      </v-row>
+    </div>
   </div>
 </template>
 
 <script>
-import _ from 'lodash';
+// import _ from 'lodash';
 export default {
   data:()=> ({
-    show: true,
-    change:true,
-    order: false,
-    count: 0,
-    list:[
-      {id:1,name:'りんご',price:100},
-      {id:2,name:'ばなな',price:200},
-      {id:3,name:'いちご',price:300}
-    ]
+    length:4,
+    // numbers:[
+    //   1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16
+    // ],
+    numbers1:[
+      {id:1,num:1,visibility:true},
+      {id:2,num:2,visibility:true},
+      {id:3,num:3,visibility:true},
+      {id:4,num:4,visibility:true},
+    ],
+    numbers2:[
+      {id:5,num:5,visibility:true},
+      {id:6,num:6,visibility:true},
+      {id:7,num:7,visibility:true},
+      {id:8,num:8,visibility:true},
+    ],
+    numbers3:[
+      {id:9,num:9,visibility:true},
+      {id:10,num:10,visibility:true},
+      {id:11,num:11,visibility:true},
+      {id:12,num:12,visibility:true},
+    ],
+    numbers4:[
+      {id:13,num:13,visibility:true},
+      {id:14,num:14,visibility:true},
+      {id:15,num:15,visibility:true},
+      {id:16,num:16,visibility:true},
+    ],
   }),
-  methods: {
-    toggleBtn: function () {
-      this.show = !this.show
-    },
-    enter:function(done){
-      console.log('enter')
-      setTimeout(done,1000)
-    },
-    afterEnter:function(){
-      console.log('afterEnter')
-    }
-  },
-  computed:{
-    sortedList:function(){
-      return _.orderBy(this.list,'price',this.order ? 'desc':'asc')
+  methods:{
+    judgeAndHide(id){
+      this.numbers1[id-1].visibility = false
     }
   }
 }
 </script>
-<style>
-.simple-enter-active,.simple-leave-active {
-  transition:opacity 1s;
+<style scoped>
+.balloon{
+  align-items:center;
+  display:flex;
+  justify-content:center;
+
 }
 
-.simple-enter,.simple-leave-to {
-  opacity:0;
+.rounded-circle {
+  border-radius: 50%;
+  width: 64px;
+  height: 64px;
+  background-color:rgba(11,10,120,0.2);
 }
-.datachange-enter-active,.datachange-leave-active {
-  transition:opacity 1s;
-}
-.datachange-leave-active{
-  position:absolute;
-}
-.datachange-enter,.datachange-leave-to{
-  opacity:0;
-}
-.group-move{
-  transition:transform 1s;
-}
-
-
 </style>
