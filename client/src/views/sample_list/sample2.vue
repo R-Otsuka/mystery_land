a2578b50c171df28e9267278d4f855cb37be6a1a
kamio06 <yakugozy@gmail.com>
2020-11-12 19:26:41 +0900

    風船クリックで背景が透明になり数字が現れる仕様を追加

--- client/src/views/sample_list/sample2.vue
+++ client/src/views/sample_list/sample2.vue
@@ -1,29 +1,37 @@
 <template>
   <v-container>
     <v-row justify="center" class="ma-5">
-      <v-col cols="3" v-for="number in numList1" v-bind:key="number.id">
-        <div class="rounded-circle mx-auto balloon">
-          <span>{{ number.num }}</span>
-        </div>
-      </v-col>
-    </v-row>
-    <v-row justify="center" class="ma-5">
-      <v-col cols="3" v-for="number in numList2" v-bind:key="number.id">
-        <div class="rounded-circle mx-auto balloon">
+      <v-col cols="3" v-for="(number, index) in numRow1" v-bind:key="number.id">
+        <div class="rounded-circle mx-auto balloon"
+             :class="[number.visibility ? 'trans':'untrans']"
+             @click="showNumber(0, index)">
           {{ number.num }}
         </div>
       </v-col>
     </v-row>
     <v-row justify="center" class="ma-5">
-      <v-col cols="3" v-for="number in numList3" v-bind:key="number.id">
-        <div class="rounded-circle mx-auto balloon">
+      <v-col cols="3" v-for="(number, index) in numRow2" v-bind:key="number.id">
+        <div class="rounded-circle mx-auto balloon"
+             :class="[number.visibility ? 'trans':'untrans']"
+             @click="showNumber(1, index)">
           {{ number.num }}
         </div>
       </v-col>
     </v-row>
     <v-row justify="center" class="ma-5">
-      <v-col cols="3" v-for="number in numList4" v-bind:key="number.id">
-        <div class="rounded-circle mx-auto balloon">
+      <v-col cols="3" v-for="(number, index) in numRow3" v-bind:key="number.id">
+        <div class="rounded-circle mx-auto balloon"
+             :class="[number.visibility ? 'trans':'untrans']"
+             @click="showNumber(2, index)">
+          {{ number.num }}
+        </div>
+      </v-col>
+    </v-row>
+    <v-row justify="center" class="ma-5">
+      <v-col cols="3" v-for="(number, index) in numRow4" v-bind:key="number.id">
+        <div class="rounded-circle mx-auto balloon"
+             :class="[number.visibility ? 'trans':'untrans']"
+             @click="showNumber(3, index)">
           {{ number.num }}
         </div>
       </v-col>
@@ -37,66 +45,59 @@ export default {
   data:()=> ({
     length:4,
     numbers:[
-      1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16
-    ],
+      {id:1,num:1,visibility:true},
+      {id:2,num:2,visibility:true},
+      {id:3,num:3,visibility:true},
+      {id:4,num:4,visibility:true},
+      {id:5,num:5,visibility:true},
+      {id:6,num:6,visibility:true},
+      {id:7,num:7,visibility:true},
+      {id:8,num:8,visibility:true},
+      {id:9,num:1,visibility:true},
+      {id:10,num:2,visibility:true},
+      {id:11,num:3,visibility:true},
+      {id:12,num:4,visibility:true},
+      {id:13,num:5,visibility:true},
+      {id:14,num:6,visibility:true},
+      {id:15,num:7,visibility:true},
+      {id:16,num:8,visibility:true}
+    ]
   }),
   computed:{
-    numList1(){
-      const num1 = this.numbers.slice(0,4);
-      let array = []
-      for(let i=0;i<4;i++){
-        if(num1[i] > 8) {
-          array.push({id: num1[i], num: num1[i]-8, visibility: true})
-        }else{
-          array.push({id: num1[i], num: num1[i], visibility: true})
-        }
-      }
-      return array
+    numRow1(){
+      let numRow = this.numbers.slice(0,4);
+      return numRow
     },
-    numList2(){
-      const num2 = this.numbers.slice(4,8);
-      let array = []
-      for(let i=0;i<4;i++){
-        if(num2[i] > 8) {
-          array.push({id: num2[i], num: num2[i]-8, visibility: true})
-        }else{
-          array.push({id: num2[i], num: num2[i], visibility: true})
-        }
-      }
-      return array
+    numRow2(){
+      let numRow = this.numbers.slice(4,8);
+      return numRow
     },
-    numList3(){
-      const num3 = this.numbers.slice(8,12);
-      let array = []
-      for(let i=0;i<4;i++){
-        if(num3[i] > 8) {
-          array.push({id: num3[i], num: num3[i]-8, visibility: true})
-        }else{
-          array.push({id: num3[i], num: num3[i], visibility: true})
-        }
-      }
-      return array
+    numRow3(){
+      let numRow = this.numbers.slice(8,12);
+      return numRow
     },
-    numList4(){
-      const num4 = this.numbers.slice(12,16);
-      let array = []
-      for(let i=0;i<4;i++){
-        if(num4[i] > 8) {
-          array.push({id: num4[i], num: num4[i]-8, visibility: true})
-        }else{
-          array.push({id: num4[i], num: num4[i], visibility: true})
-        }
-      }
-      return array
+    numRow4(){
+      const numRow = this.numbers.slice(12,16);
+      return numRow
     },
   },
   methods:{
-    listShuffle() {
+    numberShuffle() {
       this.numbers = _.shuffle(this.numbers);
     },
+    showNumber(id, index){
+      //クリック箇所の背景色を透明に。classの追加かな
+      const No = id*4+index
+      this.numbers[No].visibility = !this.numbers[No].visibility
+    }
   },
   created(){
-    this.listShuffle()
+    this.numberShuffle()
+    let numarray = []
+    for(let i=0;i<16;i++){
+      numarray.push({id:i,num:i,visibility:true})
+    }
+    return numarray
   }
 }
 </script>
@@ -112,6 +113,12 @@ export default {
   border-radius: 50%;
   width: 50px;
   height: 50px;
-  background-color:rgba(11,10,120,0.2);
 }
+.trans{
+  background-color:rgba(0,0,0,0.1);
+}
+.untrans{
+
+}
+
 </style>
