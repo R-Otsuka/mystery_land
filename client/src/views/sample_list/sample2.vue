bf051bbe42a6245266827d26315695f12625e846
kamio06 <yakugozy@gmail.com>
2020-11-17 17:37:39 +0900

    連続クリックの制御

--- client/src/views/sample_list/sample2.vue
+++ client/src/views/sample_list/sample2.vue
@@ -4,7 +4,7 @@
       <v-col cols="3" v-for="(number, index) in numRow1" v-bind:key="number.id">
         <div class="rounded-circle mx-auto balloon"
              :class="[number.visibility ? 'trans':'untrans']"
-             @click="showNumber(0, index)">
+             @click="judgeAndHide(0, index, number.num)">
           {{ number.num }}
         </div>
       </v-col>
@@ -13,7 +13,7 @@
       <v-col cols="3" v-for="(number, index) in numRow2" v-bind:key="number.id">
         <div class="rounded-circle mx-auto balloon"
              :class="[number.visibility ? 'trans':'untrans']"
-             @click="showNumber(1, index)">
+             @click="judgeAndHide(1, index, number.num)">
           {{ number.num }}
         </div>
       </v-col>
@@ -22,7 +22,7 @@
       <v-col cols="3" v-for="(number, index) in numRow3" v-bind:key="number.id">
         <div class="rounded-circle mx-auto balloon"
              :class="[number.visibility ? 'trans':'untrans']"
-             @click="showNumber(2, index)">
+             @click="judgeAndHide(2, index, number.num)">
           {{ number.num }}
         </div>
       </v-col>
@@ -31,7 +31,7 @@
       <v-col cols="3" v-for="(number, index) in numRow4" v-bind:key="number.id">
         <div class="rounded-circle mx-auto balloon"
              :class="[number.visibility ? 'trans':'untrans']"
-             @click="showNumber(3, index)">
+             @click="judgeAndHide(3, index, number.num)">
           {{ number.num }}
         </div>
       </v-col>
@@ -44,7 +44,11 @@ import _ from 'lodash';
 export default {
   data:()=> ({
     length:4,
+    opened:false,
+    openedNum:null,
+    openedNumIndex:null,
     numbers:[
+      {id:0,num:0,visibility:true},
       {id:1,num:1,visibility:true},
       {id:2,num:2,visibility:true},
       {id:3,num:3,visibility:true},
@@ -52,15 +56,14 @@ export default {
       {id:5,num:5,visibility:true},
       {id:6,num:6,visibility:true},
       {id:7,num:7,visibility:true},
-      {id:8,num:8,visibility:true},
+      {id:8,num:0,visibility:true},
       {id:9,num:1,visibility:true},
       {id:10,num:2,visibility:true},
       {id:11,num:3,visibility:true},
       {id:12,num:4,visibility:true},
       {id:13,num:5,visibility:true},
       {id:14,num:6,visibility:true},
-      {id:15,num:7,visibility:true},
-      {id:16,num:8,visibility:true}
+      {id:15,num:7,visibility:true}
     ]
   }),
   computed:{
@@ -85,11 +88,46 @@ export default {
     numberShuffle() {
       this.numbers = _.shuffle(this.numbers);
     },
-    showNumber(id, index){
+    judgeAndHide(id, index, num){
+      //クリックした風船の配列内順位
+      const arrayNo = id*4+index
+      if(this.opened == false) {
+        //1ターン目は残っている風船しかクリックできないようにする。
+        if(this.numbers[arrayNo].visibility == true) {
+          this.deleteBalloon(arrayNo)
+          this.opened = true
+          this.openedNum = num
+          this.openedNumIndex = arrayNo
+        }
+      }else{
+        if(this.openedNumIndex == arrayNo){
+          //同じ場所を二回クリック。風船を復活させる
+          this.reviveBalloon(arrayNo);
+        }else if(this.openedNum == num){
+          //hit
+          this.deleteBalloon(arrayNo)
+        }else{
+          //外れ
+          this.deleteBalloon(arrayNo)
+          setTimeout(this.reviveBalloon(arrayNo), 10000);
+          setTimeout(this.reviveBalloon(this.openedNumIndex), 10000);
+        }
+        this.opened = false
+        this.openedNum = null
+        this.openedNumIndex = null
+      }
+    },
+    deleteBalloon:function(index){
       //クリック箇所の背景色を透明に。classの追加かな
-      const No = id*4+index
-      this.numbers[No].visibility = !this.numbers[No].visibility
+      this.numbers[index].visibility = false
+    },
+    reviveBalloon:function(index){
+      //クリック箇所の背景色を戻す
+      this.numbers[index].visibility = true
+      console.log("完了")
+      console.log(index)
     }
+
   },
   created(){
     this.numberShuffle()
@@ -106,13 +144,12 @@ export default {
   align-items:center;
   display:flex;
   justify-content:center;
-
 }
-
 .rounded-circle {
   border-radius: 50%;
   width: 50px;
   height: 50px;
+  transition: all 500ms 0s ease;
 }
 .trans{
   background-color:rgba(0,0,0,0.1);
