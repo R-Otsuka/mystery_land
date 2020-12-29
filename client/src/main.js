7d944777b8eb5bcbc8a9a75bde837c71b7fdfa00
R-Otsuka <yakugozy@gmail.com>
2020-12-29 19:25:31 +0900

    デプロイ準備、パスの変更

--- client/src/main.js
+++ client/src/main.js
@@ -1,15 +1,19 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue' //packageのVue
+import App from './App.vue' //App.vueのコンポーネントファイル
 import router from './router'
 import vuetify from './plugins/vuetify';
 import axios from 'axios' //追記
 import VueAxios from 'vue-axios' //追記
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false //お助け、productionモードではfalseであるべき。開発モードではtrueでも可
 Vue.use(VueAxios, axios) //追記
 
 new Vue({
   router,
   vuetify,
-  render: h => h(App)
-}).$mount('#app')
+  // render: h => h("div","こんにちは")
+  render: h => h(App) //ES6の書き方
+  // render: function(h){ ES5の書き方
+  //   return h(App)
+  // }
+}).$mount('#app') //mount
